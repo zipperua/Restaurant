a30d7231e593949cbd145c9361258dac6d58abcf
zipperua <55282336+zipperua@users.noreply.github.com>
2021-04-24 12:08:44 +0300

    refactor classes extenf from BaseDish (#1)

--- src/MainDish.java
+++ src/MainDish.java
@@ -1,25 +1,9 @@
-public class MainDish {
-    private String mainDishName;
-    private double mainDishPrice;
+public class MainDish extends BaseDish{
 
-    MainDish(String name, double price) {
-        this.mainDishName = name;
-        this.mainDishPrice = price;
-    }
+        MainDish(String name, double price){
+            super(name, price);
+            }
 
-        public String getMainDishName() {
-        return mainDishName;
-        }
-        public void setMainDishName(String name){
-        this.mainDishName = name;
-        }
-
-        public double getMainDishPrice() {
-        return mainDishPrice;
-        }
-        public void setMainDishPrice(double price){
-        this.mainDishPrice = price;
-        }
 
 
 
